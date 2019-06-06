72a6b1c912a56ad632357e96aa17976dd8727df3
Kyle <kyleratti@users.noreply.github.com>
2019-06-05 21:23:46 -0400

    Added last endpoints for commentreply

--- src/controllers/admin/api.commentreply.ts
+++ src/controllers/admin/api.commentreply.ts
@@ -10,26 +10,28 @@ router.get("/get", async (req: Request, res: Response) => {
   authorized(req, res)
     .then(async () => {
       let reqData = req.body.data;
-      let reqUrl = reqData.reqUrl;
-      let mirroredVideo = await MirroredVideo.findOne({ url: reqUrl });
+      let reqRedditPostId = reqData.redditPostId;
+      let comment = await CommentReply.findOne({
+        redditPostId: reqRedditPostId
+      });
 
-      if (mirroredVideo)
+      if (comment)
         return response(res, {
           status: HttpStatus.OK,
           message: `OK`,
           data: {
-            id: mirroredVideo.id,
-            url: mirroredVideo.url,
-            botUsername: mirroredVideo.bot.username,
-            createdAt: mirroredVideo.createdAt,
-            updatedAt: mirroredVideo.updatedAt
+            id: comment.id,
+            redditPostId: comment.redditPostId,
+            status: comment.status,
+            createdAt: comment.createdAt,
+            updatedAt: comment.updatedAt
           }
         });
     })
     .catch(err => {
       return response(res, {
         status: HttpStatus.NOT_FOUND,
-        message: `Mirrored video not found`,
+        message: `Comment reply not found`,
         data: {
           error: err
         }
@@ -76,134 +78,33 @@ router.get("/getall", async (req: Request, res: Response) => {
     });
 });
 
-router.put("/add", async (req: Request, res) => {
-  authorized(req, res)
-    .then(async () => {
-      let reqData = req.body.data;
-      let reqUrl = reqData.url;
-      let reqBotUsername = reqData.botUsername;
-      // TODO: reddit comment ID
-
-      let bot = await RegisteredBot.findOne({ username: reqBotUsername });
-
-      let mirroredVideo = new MirroredVideo();
-      mirroredVideo.url = reqUrl;
-      mirroredVideo.bot = bot;
-      // TODO: reddit comment ID
-      mirroredVideo.save();
-
-      return response(res, {
-        status: HttpStatus.OK,
-        message: `Successfully created new mirrored video`,
-        data: {
-          url: reqUrl,
-          botUsername: bot.username
-          // TODO: reddit comment ID
-        }
-      });
-    })
-    .catch(err => {
-      return response(res, {
-        status: HttpStatus.NOT_FOUND,
-        message: `Mirrored video not found`,
-        data: {
-          error: err
-        }
-      });
-    });
-});
-
 router.post("/update", (req: Request, res) => {
   authorized(req, res).then(async () => {
     let reqData = req.body.data;
-    let reqUsername = reqData.username;
-    let reqDeveloper = reqData.developer;
-    let reqToken = reqData.token;
-    let bot = await RegisteredBot.findOne({ username: reqUsername });
-    let username = bot.username;
+    let reqRedditPostId = reqData.redditPostId;
+    let reqStatus = reqData.status;
+    let comment = await CommentReply.findOne({ redditPostId: reqRedditPostId });
 
-    let messages = [];
-    let data = {};
-
-    if (!bot)
+    if (!comment)
       return response(res, {
         status: HttpStatus.BAD_REQUEST,
-        message: `Bot does not exist`,
+        message: `Comment does not exist`,
         data: {
-          username: username
+          redditPostId: reqRedditPostId
         }
       });
 
-    if (reqDeveloper) {
-      bot.developer = reqDeveloper;
-      messages.push(`Updated developer`);
-      data["newDeveloper"] = reqDeveloper;
-    }
+    comment.status = reqStatus;
+    await comment.save();
 
-    if (reqToken) {
-      bot.token = reqToken;
-      messages.push(`Updated token`);
-      data["newToken"] = reqToken;
-    }
-
-    bot
-      .save()
-      .then(() => {
-        return response(res, {
-          status: HttpStatus.OK,
-          message: messages.join(", "),
-          data: data
-        });
-      })
-      .catch(err => {
-        return response(res, {
-          status: HttpStatus.INTERNAL_SERVER_ERROR,
-          message: `Error saving changes to bot`,
-          data: {
-            username: username,
-            error: err
-          }
-        });
-      });
-  });
-});
-
-router.delete("/delete", (req: Request, res) => {
-  authorized(req, res).then(async () => {
-    let reqData = req.body.data;
-    let username = reqData.username;
-    let bot = await RegisteredBot.findOne({ username: username });
-
-    if (!bot)
-      return response(res, {
-        status: HttpStatus.BAD_REQUEST,
-        message: `Bot does not exist`,
-        data: {
-          username: username
-        }
-      });
-
-    bot
-      .remove()
-      .then(() => {
-        return response(res, {
-          status: HttpStatus.OK,
-          message: "Successfully removed bot",
-          data: {
-            username: username
-          }
-        });
-      })
-      .catch(err => {
-        return response(res, {
-          status: HttpStatus.INTERNAL_SERVER_ERROR,
-          message: "Error removing bot",
-          data: {
-            username: username,
-            error: err
-          }
-        });
-      });
+    return response(res, {
+      status: HttpStatus.OK,
+      message: `Successfully updated comment reply`,
+      data: {
+        redditPostId: reqRedditPostId,
+        status: reqStatus
+      }
+    });
   });
 });
 
