ade155ffb912f8dbc232cc68958f088860618c4f
Kyle <kyleratti@users.noreply.github.com>
2019-10-03 21:17:15 -0400

    Changed IP to Docker Compose hostname
    This should fix the unable to connect issues

--- scripts/acm-crontab.sh
+++ scripts/acm-crontab.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -X POST -H "X-ACM-Crontab-Token: $API_CRONTAB_TOKEN" http://127.0.0.1:$PORT/cron/commentreply/sync
+curl -X POST -H "X-ACM-Crontab-Token: $API_CRONTAB_TOKEN" http://web:$PORT/cron/commentreply/sync
