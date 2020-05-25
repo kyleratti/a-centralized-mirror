4c1337fecbb5659b7bc9fdc5cc26678cf9709fa5
Kyle <kyleratti@users.noreply.github.com>
2020-05-24 22:50:14 -0400

    Added /admin/mirroredvideos/outdated endpoint

--- src/controllers/admin/commentreply.ts
+++ src/controllers/admin/commentreply.ts
@@ -3,6 +3,7 @@ import HttpStatus from "http-status-codes";
 import { authorized } from ".";
 import { response } from "..";
 import { CommentReply } from "../../entity";
+import { CommentReplyStatus } from "../../structures";
 
 const router: Router = Router();
 
@@ -14,17 +15,17 @@ router.get("/get", (req: Request, res: Response) => {
 
     try {
       comment = await CommentReply.findOne({
-        redditPostId_Parent: reqRedditPostId
+        redditPostId_Parent: reqRedditPostId,
       });
     } catch (err) {
       req.log.fatal({
         msg: `Error locating comment reply`,
-        error: err
+        error: err,
       });
 
       return response(res, {
         status: HttpStatus.INTERNAL_SERVER_ERROR,
-        message: `Error retrieving comment reply information`
+        message: `Error retrieving comment reply information`,
       });
     }
 
@@ -37,13 +38,13 @@ router.get("/get", (req: Request, res: Response) => {
           redditPostId: comment.redditPostId,
           status: comment.status,
           createdAt: comment.createdAt,
-          updatedAt: comment.updatedAt
-        }
+          updatedAt: comment.updatedAt,
+        },
       });
     else
       return response(res, {
         status: HttpStatus.NOT_FOUND,
-        message: `Comment reply not found`
+        message: `Comment reply not found`,
       });
   });
 });
@@ -55,31 +56,31 @@ router.get("/getall", (req: Request, res: Response) => {
     try {
       comments = await CommentReply.find({
         order: {
-          createdAt: "ASC"
-        }
+          createdAt: "ASC",
+        },
       });
     } catch (err) {
       req.log.fatal({
         msg: `Error locating comment reply`,
-        error: err
+        error: err,
       });
 
       return response(res, {
         status: HttpStatus.INTERNAL_SERVER_ERROR,
-        message: `Error retrieving comment replies information`
+        message: `Error retrieving comment replies information`,
       });
     }
 
     let commentsData = [];
 
-    comments.forEach(comment => {
+    comments.forEach((comment) => {
       commentsData.push({
         id: comment.id,
         redditPostId_Parent: comment.redditPostId_Parent,
         redditPostId_Reply: comment.redditPostId_Reply,
         status: comment.status,
         createdAt: comment.createdAt,
-        updatedAt: comment.updatedAt
+        updatedAt: comment.updatedAt,
       });
     });
 
@@ -88,8 +89,45 @@ router.get("/getall", (req: Request, res: Response) => {
       message: `OK`,
       data: {
         count: commentsData.length,
-        commentReplies: commentsData
-      }
+        commentReplies: commentsData,
+      },
+    });
+  });
+});
+
+router.get("/outdated", (req: Request, res: Response) => {
+  authorized(req, res, async () => {
+    let comments: CommentReply[];
+
+    try {
+      comments = await CommentReply.find({
+        where: {
+          status: CommentReplyStatus.Outdated,
+        },
+        order: {
+          updatedAt: "ASC",
+        },
+        take: 10, // A limit is specified as not to launch a mini-DoS attack against reddit's API
+      });
+    } catch (err) {
+      req.log.fatal(`Error retrieving outdated comment replies`);
+
+      return response(res, {
+        status: HttpStatus.INTERNAL_SERVER_ERROR,
+        message: `Error retrieving outdated comment replies`,
+      });
+    }
+
+    if (!comments || comments.length <= 0)
+      return response(res, {
+        status: HttpStatus.OK,
+        message: `All comments are up-to-date`,
+      });
+
+    return response(res, {
+      status: HttpStatus.OK,
+      message: `Comments are awaiting processing`,
+      data: comments,
     });
   });
 });
@@ -103,17 +141,17 @@ router.post("/update", (req: Request, res) => {
 
     try {
       comment = await CommentReply.findOne({
-        redditPostId_Parent: reqRedditPostId
+        redditPostId_Parent: reqRedditPostId,
       });
     } catch (err) {
       req.log.fatal({
         msg: `Error locating comment reply`,
-        error: err
+        error: err,
       });
 
       return response(res, {
         status: HttpStatus.INTERNAL_SERVER_ERROR,
-        message: `Error retrieving comment reply information`
+        message: `Error retrieving comment reply information`,
       });
     }
 
@@ -122,8 +160,8 @@ router.post("/update", (req: Request, res) => {
         status: HttpStatus.BAD_REQUEST,
         message: `Comment does not exist`,
         data: {
-          redditPostId: reqRedditPostId
-        }
+          redditPostId: reqRedditPostId,
+        },
       });
 
     comment.status = reqStatus;
@@ -133,12 +171,12 @@ router.post("/update", (req: Request, res) => {
     } catch (err) {
       req.log.fatal({
         msg: `Error saving updated comment`,
-        error: err
+        error: err,
       });
 
       return response(res, {
         status: HttpStatus.INTERNAL_SERVER_ERROR,
-        message: `Error saving updated comment`
+        message: `Error saving updated comment`,
       });
     }
 
@@ -147,8 +185,8 @@ router.post("/update", (req: Request, res) => {
       message: `Successfully updated comment reply`,
       data: {
         redditPostId: reqRedditPostId,
-        status: reqStatus
-      }
+        status: reqStatus,
+      },
     });
   });
 });
