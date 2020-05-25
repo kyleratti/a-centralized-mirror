4f76d5c428442a90ef97379c140efee50fc546b2
Kyle <kyleratti@users.noreply.github.com>
2020-05-25 00:05:34 -0400

    Added CommentReplyStatus.Expired

--- src/structures/commentreply.ts
+++ src/structures/commentreply.ts
@@ -2,5 +2,7 @@ export enum CommentReplyStatus {
   /** Indicates the comment is current and not in need of updates */
   Current,
   /** Indicates the comment is outdated and needs to be updated */
-  Outdated
+  Outdated,
+  /** Indicates the parent is expired and cannot be updated anymore */
+  Expired,
 }
