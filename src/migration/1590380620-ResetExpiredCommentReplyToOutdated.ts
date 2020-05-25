c3f9f497ca06cdadcb1d51a71b3b5a25ea31232b
Kyle <kyleratti@users.noreply.github.com>
2020-05-25 00:23:56 -0400

    Fix query

--- src/migration/1590380620-ResetExpiredCommentReplyToOutdated.ts
+++ src/migration/1590380620-ResetExpiredCommentReplyToOutdated.ts
@@ -1,10 +1,10 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class ResetExpiredCommentReplyToOutdated1590380368
+export class ResetExpiredCommentReplyToOutdated1590380620
   implements MigrationInterface {
   async up(queryRunner: QueryRunner): Promise<any> {
     await queryRunner.query(
-      `UPDATE commentreply SET status = 1 WHERE status = 2`
+      `UPDATE comment_reply SET status = 1 WHERE status = 2`
     );
   }
 
