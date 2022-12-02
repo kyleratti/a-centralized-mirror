92097cd04268004bf2813df47fcb28a871539d72
Kyle Ratti <kyleratti@users.noreply.github.com>
2022-12-02 00:10:01 -0500

    feat: add test endpoint for reddit user

---
+++ WebApi/Controllers/Admin/RedditTestController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SnooBrowser.Browsers;
+
+#pragma warning disable CS1591
+namespace WebApi.Controllers.Admin;
+
+[Route("v1/admin/reddit-test")]
+public class RedditTestController : AdminApiController
+{
+	private readonly MeBrowser _meBrowser;
+
+	/// <inheritdoc />
+	public RedditTestController(IServiceProvider serviceProvider, MeBrowser meBrowser) : base(serviceProvider)
+	{
+		_meBrowser = meBrowser;
+	}
+
+	[HttpGet]
+	[Route("get-reddit-user")]
+	public async Task<object> GetRedditUser()
+	{
+		var result = await _meBrowser.GetMe();
+
+		return new
+		{
+			username = result.Username,
+			isMod = result.IsMod
+		};
+	}
+}
