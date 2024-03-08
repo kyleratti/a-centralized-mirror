13208bb734d0a3234a1e935cba9a049a79c4ba58
Kyle Ratti <kyleratti@users.noreply.github.com>
2024-03-07 23:07:11 -0500

    feat: add processing queue admin endpoint

---
+++ WebApi/Controllers/Admin/AdminQueueController.cs
@@ -0,0 +1,45 @@
+﻿using ApplicationData.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers.Admin;
+
+/// <summary>
+/// Admin Queue controller
+/// </summary>
+[Route("v1/admin/queue")]
+public class AdminQueueController : AdminApiController
+{
+	private readonly LinkProvider _linkProvider;
+
+	/// <inheritdoc />
+	public AdminQueueController(
+		IServiceProvider serviceProvider,
+		LinkProvider linkProvider
+	) : base(serviceProvider)
+	{
+		_linkProvider = linkProvider;
+	}
+
+	/// <summary>
+	/// Retrieve the processing queue
+	/// </summary>
+	[HttpGet]
+	[Route("")]
+	public async Task<IActionResult> GetProcessingQueue(CancellationToken cancellationToken)
+	{
+		var postIdsWithPendingChanges = await _linkProvider.GetRedditPostIdsWithPendingChanges(cancellationToken)
+			.Select(x => new
+			{
+				queuedItemId = x.QueuedItemId,
+				redditPostId = x.RedditPostId,
+				queuedAt = x.QueuedAt,
+			})
+			.ToArrayAsync(cancellationToken);
+
+		return new OkObjectResult(new
+		{
+			itemCount = postIdsWithPendingChanges.Length,
+			queuedItems = postIdsWithPendingChanges,
+		});
+	}
+}
