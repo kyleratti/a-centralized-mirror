8de0ee21d2e40d9b3223b6a15f0f3bd5c453e165
Kyle Ratti <kyleratti@users.noreply.github.com>
2024-08-24 11:59:14 -0400

    chore: XML doc warnings

--- WebApi/Pages/Error.cshtml.cs
+++ WebApi/Pages/Error.cshtml.cs
@@ -4,21 +4,26 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace WebApi.Pages;
 
+/// <summary>
+/// Error model
+/// </summary>
 [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 [IgnoreAntiforgeryToken]
 public class ErrorModel : PageModel
 {
+	/// <summary>
+	/// Request id
+	/// </summary>
 	public string? RequestId { get; set; }
 
+	/// <summary>
+	/// Show request id
+	/// </summary>
 	public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
-	private readonly ILogger<ErrorModel> _logger;
-
-	public ErrorModel(ILogger<ErrorModel> logger)
-	{
-		_logger = logger;
-	}
-
+	/// <summary>
+	/// GET
+	/// </summary>
 	public void OnGet()
 	{
 		RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
