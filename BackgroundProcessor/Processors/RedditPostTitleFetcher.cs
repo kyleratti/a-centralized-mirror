5ca259c4831fa0625dd03d423592f6a0cecf126d
Kyle Ratti <kyleratti@users.noreply.github.com>
2024-08-27 20:21:17 -0400

    fix: skip submissions that throw

--- BackgroundProcessor/Processors/RedditPostTitleFetcher.cs
+++ BackgroundProcessor/Processors/RedditPostTitleFetcher.cs
@@ -31,14 +31,25 @@ public class RedditPostTitleFetcher : IBackgroundProcessor
 
 		await foreach (var redditPostId in query)
 		{
-			if (!(await _submissionBrowser.GetSubmission(LinkThing.CreateFromShortId(redditPostId))).Try(out var submission))
+			try
 			{
-				await _redditPostProvider.SetTitleFetchedForPostId(redditPostId, cancellationToken);
-				_logger.LogInformation("Submission title couldn't be fetched for {RedditPostId} because the submission has been removed", redditPostId);
-				return;
-			}
+				var submissionResult = await _submissionBrowser.GetSubmission(LinkThing.CreateFromShortId(redditPostId));
 
-			await _redditPostProvider.SetPostTitle(redditPostId, submission.Title, cancellationToken);
+				if (!submissionResult.Try(out var submission))
+				{
+					await _redditPostProvider.SetTitleFetchedForPostId(redditPostId, cancellationToken);
+					_logger.LogInformation("Submission title couldn't be fetched for {RedditPostId} because the submission has been removed", redditPostId);
+					return;
+				}
+
+				await _redditPostProvider.SetPostTitle(redditPostId, submission.Title, cancellationToken);
+			}
+			catch (Exception ex)
+			{
+				// We'll figure out how to clean these up later
+				_logger.LogError(ex, "Error fetching title for {RedditPostId}", redditPostId);
+				await _redditPostProvider.SetTitleFetchedForPostId(redditPostId, cancellationToken);
+			}
 		}
 	}
 }
