8eb284988c56b52823b1a1ac3bab45e4bf6c8345
Kyle Ratti <kyleratti@users.noreply.github.com>
2024-08-30 00:07:25 -0400

    fix: set titles as fetched if title is present

---
+++ WebApi/Scripts/Script_2024_08_30_01_SetTitlesAsFetchedIfTitleIsPresent.cs
@@ -0,0 +1,13 @@
+﻿using FruityFoundation.DataAccess.Abstractions;
+
+namespace WebApi.Scripts;
+
+///Script_2024_08_30_01_SetTitlesAsFetchedIfTitleIsPresent
+public class Script_2024_08_30_01_SetTitlesAsFetchedIfTitleIsPresent : IDbMaintenanceScript
+{
+	/// <inheritdoc />
+	public async Task Run(IDatabaseConnection<ReadWrite> dbConnection)
+	{
+		await dbConnection.Execute("UPDATE reddit_posts SET is_title_fetched = 1 WHERE post_title IS NOT NULL");
+	}
+}
