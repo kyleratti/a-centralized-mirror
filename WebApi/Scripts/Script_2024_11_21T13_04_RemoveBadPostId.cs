36c2331a251e811f7b01bf29e995567fd726b022
Kyle Ratti <kyleratti@users.noreply.github.com>
2024-11-21 13:05:08 -0500

    fix: remove bad post
    This was seemingly deleted by reddit and is causing errors.
    
    Fixes A-CENTRALIZED-MIRROR-1E
    Fixes A-CENTRALIZED-MIRROR-1D

---
+++ WebApi/Scripts/Script_2024_11_21T13_04_RemoveBadPostId.cs
@@ -0,0 +1,31 @@
+﻿using FruityFoundation.DataAccess.Abstractions;
+
+namespace WebApi.Scripts;
+
+/// <summary>
+/// Script_2024_11_21T13_04_RemoveBadPostId
+/// </summary>
+public class Script_2024_11_21T13_04_RemoveBadPostId : IDbMaintenanceScript
+{
+	/// <inheritdoc />
+	public async Task Run(IDatabaseConnection<ReadWrite> dbConnection)
+	{
+		await dbConnection.Execute(
+			"""
+			BEGIN TRANSACTION;
+			DELETE FROM reddit_comments WHERE reddit_post_id = '1gwjh0b';
+			DELETE FROM link_queue WHERE reddit_post_id = '1gwjh0b';
+			DELETE FROM links WHERE reddit_post_id = '1gwjh0b';
+			COMMIT;
+			""");
+
+		await dbConnection.Execute(
+			"""
+			BEGIN TRANSACTION;
+			DELETE FROM reddit_comments WHERE reddit_post_id = '1gg50tl';
+			DELETE FROM link_queue WHERE reddit_post_id = '1gg50tl';
+			DELETE FROM links WHERE reddit_post_id = '1gg50tl';
+			COMMIT;
+			""");
+	}
+}
