c994e265b3352cf97b0aa9acbd23ba67d08d0060
Kyle <kyleratti@users.noreply.github.com>
2024-11-20 19:25:48 -0500

    fix: remove bad link
    This post always returns a server error from Reddit's API. The post is locked, so it's easiest just to remove it.
    
    fixes A-CENTRALIZED-MIRROR-1D

---
+++ WebApi/Scripts/Script_2024_11_20T19_15_RemoveBadPostId.cs
@@ -0,0 +1,22 @@
+﻿using FruityFoundation.DataAccess.Abstractions;
+
+namespace WebApi.Scripts;
+
+/// <summary>
+/// Script_2024_11_20T19_15_RemoveBadPostId
+/// </summary>
+public class Script_2024_11_20T19_15_RemoveBadPostId : IDbMaintenanceScript
+{
+	/// <inheritdoc />
+	public async Task Run(IDatabaseConnection<ReadWrite> dbConnection)
+	{
+		await dbConnection.Execute(
+			"""
+			BEGIN TRANSACTION;
+			DELETE FROM reddit_comments WHERE reddit_post_id = '1gfmn96';
+			DELETE FROM link_queue WHERE reddit_post_id = '1gfmn96';
+			DELETE FROM links WHERE reddit_post_id = '1gfmn96';
+			COMMIT;
+			""");
+	}
+}
