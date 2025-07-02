c8adc4c85646c0e611132b4a84fefc4d07a430d5
Kyle Ratti <kyleratti@users.noreply.github.com>
2025-07-02 09:17:21 -0400

    fix: remove bad post

---
+++ WebApi/Scripts/Script_2025_07_02T09_17_RemoveBadPostIds.cs
@@ -0,0 +1,31 @@
+﻿using FruityFoundation.DataAccess.Abstractions;
+
+namespace WebApi.Scripts;
+
+/// <summary>
+/// Script_2025_07_02T09_17_RemoveBadPostIds
+/// </summary>
+public class Script_2025_07_02T09_17_RemoveBadPostIds : IDbMaintenanceScript
+{
+	/// <inheritdoc />
+	public async Task Run(IDatabaseConnection<ReadWrite> dbConnection)
+	{
+		string[] postIds = [
+			"1laudfi",
+			"1lcrpcw",
+			"1l9mq2t",
+		];
+
+		foreach (var postId in postIds)
+		{
+			await dbConnection.Execute(
+				"""
+				BEGIN TRANSACTION;
+				DELETE FROM reddit_comments WHERE reddit_post_id = @postId;
+				DELETE FROM link_queue WHERE reddit_post_id = @postId;
+				DELETE FROM links WHERE reddit_post_id = @postId;
+				COMMIT;
+				""", new { postId }, CancellationToken.None);
+		}
+	}
+}
